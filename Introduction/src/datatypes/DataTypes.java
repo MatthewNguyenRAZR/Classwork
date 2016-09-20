2bc46a225b85aa8ba2bcdaa543e559699ce4bddb
Student 6 <Student 6@10.8.33.171>
2016-09-20 12:33:26 -0400

    9/20/16 Update

--- Introduction/src/datatypes/DataTypes.java
+++ Introduction/src/datatypes/DataTypes.java
@@ -3,8 +3,18 @@ package datatypes;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		System.out.println(5.0/2);//double divided by int
-		System.out.println((double)5/2);//casting
-		System.out.println(5/2);//double divided by int
+		//System.out.println(5.0/2);//double divided by int
+		//System.out.println((double)5/2);//casting
+		//System.out.println(5/2);//double divided by int
+		String a = "hi "+1;
+		System.out.println(a);
+		
+		String b = "Happy "+18+"th birthday!";
+		System.out.println(b);
+			
+		String c = "Happy "+54/3+"th birthday!";
+		System.out.println(c);
 	}
+	
+	
 }
