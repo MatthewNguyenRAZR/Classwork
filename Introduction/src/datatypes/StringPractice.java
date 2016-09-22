7a313100c1cdc68138fca5c15f7b6c0ce1d4999d
Student 6 <Student 6@10.8.33.171>
2016-09-22 12:34:05 -0400

    9/22/16 Update

--- Introduction/src/datatypes/StringPractice.java
+++ Introduction/src/datatypes/StringPractice.java
@@ -1,8 +1,41 @@
 package datatypes;
-
+import java.util.Scanner;
 public class StringPractice {
-
+	static Scanner input;
+	static String user;
+	static int lineCount;
 	public static void main(String[] args) {
+		createAScanner();
+		lineCount = 0;
+		demonstrateStringMethods();
+		promptName();
+		talkForever();
+	}
+
+	public static void promptName(){
+		print("Hello, human! I am a board covered with semiconductors \nand other such "
+				+ "electronic components, What is your name?");
+		user = input.nextLine();
+		print("Awesome, I will call you "+user+" until you terminate me.");
+	}
+	public static void talkForever(){
+		while(true){
+			promptInput();
+		}
+	}
+	public static void print(String s){
+		lineCount++;
+		System.out.println("Line #"+lineCount+": "+s);
+	}
+	public static void promptInput(){
+		print(user+", type an input.");
+		String userInput = input.nextLine();
+		print("You typed: "+userInput);
+	}
+	public static void createAScanner(){
+			input = new Scanner(System.in);
+	}
+	public static void demonstrateStringMethods(){
 		//String text = new String("Hello World"); //same thing
 		String text1 = "Hello World";
 		String text2 = "Hello ";
@@ -13,6 +46,7 @@ public class StringPractice {
 		else
 			System.out.println("These strings are not equal.");
 		
+		//they look the same but are not the same:
 		System.out.println(text1);
 		System.out.println(text2+text3);
 		
@@ -22,5 +56,4 @@ public class StringPractice {
 			System.out.println("Word1 is before word2."+" lexicongraphically");
 		}
 	}
-
 }
