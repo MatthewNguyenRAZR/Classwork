179f2fd471b70ca5ffc0db441bc5de8a29fa9d25
Student 6 <Student 6@10.8.33.171>
2016-10-31 12:05:41 -0400

    caveExplorer

---
+++ Introduction/src/caveExplorer6/TwoDArrayIntro.java
@@ -0,0 +1,13 @@
+package caveExplorer6;
+
+import java.util.Arrays;
+
+public class TwoDArrayIntro {
+
+	public static void main(String[] args) {
+		int[] arr = {0,1,2,3,4,5,6,7};
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
