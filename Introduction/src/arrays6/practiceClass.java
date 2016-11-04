832ece8756fc524eff9df63b1995b457f7a6ba09
Student 6 <Student 6@10.8.33.171>
2016-11-04 13:17:16 -0400

    Update

--- Introduction/src/arrays6/practiceClass.java
+++ Introduction/src/arrays6/practiceClass.java
@@ -5,6 +5,54 @@ public class practiceClass {
 	
 	
 	public static void main(String[] args){
+		//generateMineSweeper();
+		printMap(3,4);
+	}
+	private static void printMap(int row, int col){
+		//generates top border
+		for(int top = 0;top<row;top++){
+			if(top<row-1){
+				System.out.print("____");
+			}else{
+				System.out.println("____");
+			}
+		}
+		for(int c = 0;c < col;c++){
+			//left and right and bottom of room walls
+			for(int repeat = 0;repeat < 3;repeat++){
+				for(int r = 0;r < row;r++){
+					if(repeat == 2){
+						if(r<row-1){
+							System.out.print("|___");
+						}else{
+							System.out.println("|___|");
+						}
+					}else{
+						if(r<row-1){
+							System.out.print("|   ");
+						}else{
+							System.out.println("|   |");
+						}
+					}
+					
+				}
+			}
+			//top and bottom of room walls
+			/*
+			for(int r = 0;r < row;r++){
+				if(r<row-1){
+					System.out.print("____");
+				}else{
+					System.out.println("____");
+				}
+			}
+			*/
+		}
+		
+	}
+	
+	
+	private static void generateMineSweeper(){
 		boolean[][] mines = new boolean[6][6];
 		createMines(mines,10);
 		String[][] field = new String[mines.length][mines[0].length];
@@ -21,7 +69,6 @@ public class practiceClass {
 			}
 		}
 	}
-	
 	private static void matchValues(String[][] field, boolean[][] mines) {
 		for(int row = 0;row < field.length;row++){
 			for(int col = 0;col < field[0].length;col++){
@@ -34,11 +81,11 @@ public class practiceClass {
 		}
 		
 	}
-	
 	private static String countAdjacent(boolean[][] mines, int r, int c) {
 		//r and c represent coordinates of element we are providing a String for
 		int count = 0;
 		//loop through row above to row below
+		/*
 		for(int row = r-1;row <= r+1;row++){
 			//loop through col left to col right
 			for(int col = c-1;col <= c+1;col++){
@@ -50,8 +97,45 @@ public class practiceClass {
 				}
 			}
 		}
-		return null;
+		*/
+		//this method checks elements in [][]
+		//not necessary to verify they are valid
+		/*
+		for(int row = 0;row <mines.length;row++){
+			for(int col = 0;col <mines[row].length;row++){	
+				if(Math.abs(row-r)+Math.abs(col-c) == 1 && mines[row][col]){
+					//check if true
+					count++;
+				}
+				
+			}
+		}
+		*/
+		//method is helpful when wanting to be very specific
+		//above
+		count+=validAndTrue(mines, r-1, c);
+		//below
+		count+=validAndTrue(mines, r+1, c);
+		//right
+		count+=validAndTrue(mines, r, c+1);
+		//left
+		count+=validAndTrue(mines, r+1, c-1);
+		return count+"";
 	}
+	private static int validAndTrue(boolean[][] mines, int i, int j) {
+		//check valid
+		if(i>=0 && i< mines.length && j>=0 && j < mines[0].length && mines[i][j]){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	
+	
+	
+	
+	
+	
 	public static void printBackgroundBorders(){
 		String[][] pic = new String[10][8];
 		for(int row = 0;row < pic.length;row++){
--- Introduction/src/caveExplorer6/TwoDArrayIntro.java
+++ Introduction/src/caveExplorer6/TwoDArrayIntro.java
@@ -1,27 +1,85 @@
 package caveExplorer6;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TwoDArrayIntro {
-
+	
+	public static String[][] arr2D;
+	public static String[][] pic;
+	public static int i;
+	public static int j;
+	public static Scanner in;
+	
 	public static void main(String[] args) {
-		int[] arr = {0,1,2,3,4,5,6,7};
-		System.out.println(Arrays.toString(arr));
+		//int[] arr = {0,1,2,3,4,5,6,7};
+		//System.out.println(Arrays.toString(arr));
 		 
 		//rows, columns
 		//rows = arr2D.length;
 		//columns = arr2D[0].length;
-		String[][] arr2D = new String[4][3];
+			
 		//iterate row by row
+		arr2D = new String[5][4];
+		pic = new String[5][4];
+		
 		for(int row = 0;row<arr2D.length;row++){
 			for(int col = 0;col<arr2D[row].length;col++){
 				arr2D[row][col] = "("+row+","+col+")";
 			}
 		}
-		
+		i = 2;
+		j = 3;
+		in = new Scanner(System.in);
 		//print each row
+		/*
 		for(int row = 0;row<arr2D.length;row++){
 			System.out.println(Arrays.toString(arr2D[row]));
+		}
+		*/
+		
+		startExploring();
+	}
+	public static void startExploring() {
+		while(true){
+			printPic(pic);
+			System.out.println("You are in room"+arr2D[i][j]);
+			System.out.println("What do you want to do?");
+			String input = in.nextLine();
+			while(!isValid(input)){
+				System.out.println("Please enter w, a, s, d.");
+				input = in.nextLine();
+			}
+			interpretInput(input.toLowerCase());
+		}
+	}
+	private static void interpretInput(String input) {
+		int iOrig = i;
+		int jOrig = j;
+		
+		if(input.equals("w") && i>0)i--;
+		else if(input.equals("a") && j>0)j--;
+		else if(input.equals("s") && i<arr2D.length-1)i++;
+		else if(input.equals("d") && j<arr2D[0].length-1)j++;
+		
+		if(iOrig==i&&jOrig==j){
+			System.out.println("You can no longer move farther in that direction.");
+		}
+	}
+	private static boolean isValid(String input) {
+		String lc = input.toLowerCase();
+		String[] keys = {"w","a","s","d"};
+		for(String key:keys){
+			if(key.equals(lc))return true;
+		}
+		return false;
+	}
+	public static void printPic(String[][] pic){
+		for(int row = 0;row < pic.length;row++){
+			for(int col = 0;col < pic[row].length;col++){
+				System.out.print(pic[row][col]);
+			}
+			System.out.println();
 		}
 	}
 
