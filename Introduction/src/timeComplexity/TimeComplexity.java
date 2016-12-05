5e49a8aad5e31484cb64b07a3e5b6c2bc81071c9
Student 6 <Student 6@10.8.33.171>
2016-12-05 12:29:13 -0500

    Time Complexity

--- Introduction/src/timeComplexity/TimeComplexity.java
+++ Introduction/src/timeComplexity/TimeComplexity.java
@@ -16,18 +16,28 @@ public class TimeComplexity {
 		}
 		return sum/values.length;
 	}
+	public boolean isPrime(int n){
+		boolean prime = true;					//1
+		for(int i = 2; i<n; i++){				//2(n+1-2) = 2n-1
+			if(n%i==0) prime = false;  			//2(n-2)
+		}
+		return prime;							//1
+	}
+	
 	public boolean isPrime1(int n){
-		boolean prime = true;	//1ms
-		for(int i = 2; i<n; i++){	//2(n+1-2) = 2n-1
-			if(n%i==0) prime = false;   //2(n-2)ms
+		boolean prime = true;					//1
+		for(int i = 2; i<Math.sqrt(n); i++){	//3(sqrt(n)-2+1)
+			if(n%i==0) prime = false;			//BestCase 2(sqrt(n)-2)   	Worst Case 3(sqrt(n)-2)ms 
 		}
-		return prime;	//1ms
-	}
+			return prime;						//1
+	}											//Best Case 5sqrt(n)-5		Worst Case 6sqrt(n)-7
+	
 	public boolean isPrime2(int n){
-		boolean prime = true;	//1ms
-		for(int i = 2; i<Math.sqrt(n); i++){	//n+sqrt(n)-2
-			if(n%i==0) prime = false;	//2sqrt(n)ms   
+		boolean prime = true;					//1
+		double sqrt = Math.sqrt(n);				//2
+		for(int i = 2; i<sqrt; i++){			//2(sqrt(n)-1)
+			if(n%i==0) prime = false;   		//Best Case 2(sqrt(n)-2)	Worst Case 3(sqrt(n)-2)
 		}
-			return prime;	//1ms
+		return prime;							//1
 	}
-}
+}												//Best Case 4sqrt(n)-2		Worst Case 5sqrt(n)-4
