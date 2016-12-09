f79ac4f64fe683b5de982ec88f86e45061df752e
Student 6 <Student 6@10.8.33.171>
2016-12-09 12:26:44 -0500

    Update

--- Introduction/src/gui/GUIApplication.java
+++ Introduction/src/gui/GUIApplication.java
@@ -1,8 +1,13 @@
 package gui;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame{
+	//FIELDS
+	private Screen currentScreen;
+	
 	
 	//demo purposes only
 	public static void main(String[] args){
@@ -11,8 +16,17 @@ public class GUIApplication extends JFrame{
 	public GUIApplication(int width, int height) {
 		super();
 		setBounds(20, 20, width, height);
+		//terminates program when window is closed
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initScreen();
 		setVisible(true);
+	}
+	public void initScreen() {
+		currentScreen = new Screen(getWidth(), getHeight());
+		
+	}
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
 	}
 	
 }
--- Introduction/src/gui/Screen.java
+++ Introduction/src/gui/Screen.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Screen {
@@ -8,10 +10,19 @@ public class Screen {
 	public Screen(int width,int height) {
 		initImage(width, height);
 	}
-
 	public void initImage(int width, int height) {
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		
+		update();
 	}
-
+	public BufferedImage getImage(){
+		return image;
+	}
+	public void update() {
+		Graphics2D g = image.createGraphics();
+		g.setColor(Color.PINK);
+		g.fillRect(0, 0, image.getWidth(), image.getHeight());
+	}
+	
+	
+	
 }
