c9f128dcfd75d10bf0adee22af15113375d8418f
Student 6 <Student 6@10.8.33.171>
2016-12-12 13:09:31 -0500

    update

--- Introduction/src/gui/components/Component.java
+++ Introduction/src/gui/components/Component.java
@@ -49,6 +49,7 @@ public abstract class Component implements Visible {
 	
 	public abstract void update(Graphics2D g);
 	
+	//delete image to start over
 	public Graphics2D clear(){
 		image = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
 		return image.createGraphics();
