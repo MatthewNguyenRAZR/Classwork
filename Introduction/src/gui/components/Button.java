716b735f84b121ef94368eb9173d5cf51cdb1004
Student 6 <Student 6@10.8.33.171>
2016-12-16 12:39:08 -0500

    Update

--- Introduction/src/gui/components/Button.java
+++ Introduction/src/gui/components/Button.java
@@ -2,65 +2,62 @@ package gui.components;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
-public class Button extends TextLabel {
+public class Button extends TextLabel implements Clickable{
+
 	private Color color;
 	private Action action;
 	
 	public Button(int x, int y, int w, int h, String text, Color color, Action action) {
 		super(x, y, w, h, text);
-		/*
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.text = text;
-		*/
 		this.color = color;
 		this.action = action;
 		update();
 	}
 	
-	public Color getColor() {
+	public Color getColor(){
 		return color;
 	}
-
-	public void setColor(Color color) {
-		this.color = color;
+	
+	public void setColor(Color c){
+		color = c;
 		update();
 	}
-
-	public Action getAction() {
-		return action;
-	}
-
-	public void setAction(Action action) {
-		this.action = action;
-		update();
-	}
-
-	@Override
-	public void update(Graphics2D g) {
-		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	
+	public void update(Graphics2D g){
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(color);
 		g.fillRoundRect(0, 0, getWidth(), getHeight(), 35, 25);
-		g.setColor(Color.BLACK);
-		//g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 25);
-		g.drawRoundRect(0, 0, getWidth(), getHeight(), 35, 25);
-		g.setColor(Color.WHITE);
-		g.setFont(new Font(this.getFont(),Font.PLAIN,this.getSize()));
-		if(this.getText() != null) g.drawString(this.getText(), 4, getHeight()-5);
+		g.setColor(Color.black);
+		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 25);
+		g.setFont(new Font(getFont(),Font.PLAIN,getSize()));
+		FontMetrics fm = g.getFontMetrics();
+		
+		if(getText()!= null){
+			g.setColor(Color.white);
+			String t = getText();
+			//just in case text is too wide, cut off
+			int cutoff = t.length();
+			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+				cutoff --;
+				t = t.substring(0,cutoff); 
+			}
+			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, 
+					(getHeight()+fm.getHeight()-fm.getDescent())/2);
+		}
 	}
-	/*
-	@Override
-	public boolean isHovered(int x, int y){
-		return x > getX() && x < getX()+getWidth() && y > getY() && y < getY()+getHeight()
+
+
+	public boolean isHovered(int x, int y) {
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
-	*/
-	@Override
+	
 	public void act(){
 		action.act();
 	}
+
 }
--- Introduction/src/gui/components/Visible.java
+++ Introduction/src/gui/components/Visible.java
@@ -10,4 +10,6 @@ public abstract interface Visible {
 	public int getHeight();
 	public boolean isAnimated();
 	public void update();
+	boolean isHovered(int x, int y);
+	void act();
 }
