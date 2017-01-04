7d29655b8e42b625edb30013ec8ee7d21fc79690
Student 6 <Student 6@10.8.33.100>
2017-01-04 12:35:15 -0500

    Update

--- Introduction/src/gui/whackAMole/MoleInterface.java
+++ Introduction/src/gui/whackAMole/MoleInterface.java
@@ -8,6 +8,7 @@ import gui.components.Clickable;
 /**
  * @author Student 6
  * this is for my partner, the Enemy Designer to implement
+ * 
  */
 public interface MoleInterface extends Clickable {
 
