03fc7b8c337c9fe8d3e64c83cc2ca876cff819ba
Student 6 <Student 6@10.8.33.100>
2017-01-06 12:39:55 -0500

    Update

---
+++ Introduction/src/gui/simon/SimonScreenX.java
@@ -0,0 +1,34 @@
+package gui.simon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gui.components.Visible;
+import gui.screens.ClickableScreen;
+
+public class SimonScreenX extends ClickableScreen implements Runnable {
+
+	public SimonScreenX(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
