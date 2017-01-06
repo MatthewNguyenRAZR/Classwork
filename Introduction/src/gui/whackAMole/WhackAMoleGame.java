db26c5a18cb5537ad77c1f744ba3f41e4c77a108
Student 6 <Student 6@10.8.33.100>
2017-01-06 12:47:54 -0500

    Update

--- Introduction/src/gui/whackAMole/WhackAMoleGame.java
+++ Introduction/src/gui/whackAMole/WhackAMoleGame.java
@@ -6,7 +6,6 @@ public class WhackAMoleGame extends GUIApplication{
 	
 	public WhackAMoleGame(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
