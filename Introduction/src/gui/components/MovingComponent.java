a9f2ac4055cbfbf76ea04ed2cbdeb08aa3ad3282
Student 6 <Student 6@10.8.33.100>
2017-01-11 12:04:32 -0500

    Update

---
+++ Introduction/src/gui/components/MovingComponent.java
@@ -0,0 +1,9 @@
+package gui.components;
+
+public class MovingComponent {
+
+	public MovingComponent() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Introduction/src/gui/simon/MoveInterfaceMatthew.java
+++ Introduction/src/gui/simon/MoveInterfaceMatthew.java
@@ -1,5 +1,5 @@
 package gui.simon;
 
 public interface MoveInterfaceMatthew {
-
+	ButtonInterfaceMatthew getButton();
 }
