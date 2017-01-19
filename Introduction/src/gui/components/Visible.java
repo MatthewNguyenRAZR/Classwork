957442ba9399e4a40b55e685c7bc4c60ba0ad86c
Teacher <Teacher@10.148.46.144>
2017-01-19 14:39:05 -0500

    Major Final Update

--- Introduction/src/gui/components/Visible.java
+++ Introduction/src/gui/components/Visible.java
@@ -6,7 +6,9 @@ public interface Visible {
 
 	public BufferedImage getImage();
 	public int getX();
-	public int getY();
+	public int getY();	
+	public void setX(int x);
+	public void setY(int y);
 	public int getWidth();
 	public int getHeight();
 	public boolean isAnimated();
