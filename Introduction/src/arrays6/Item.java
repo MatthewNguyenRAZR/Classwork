2addf7860654bbd98ca84d8a81dec6aa94614df0
Student 6 <Student 6@10.8.33.100>
2017-03-06 13:13:13 -0500

    ArrayList

---
+++ Introduction/src/arrays6/Item.java
@@ -0,0 +1,26 @@
+package arrays6;
+
+public class Item {
+
+	private String description;
+	private boolean purchased;
+	
+	public Item(String d) {
+		this.description = d;
+		this.purchased = false;
+	}
+	public void setPurchase(boolean b){
+		this.purchased = b;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public boolean isPurchased() {
+		return purchased;
+	}
+	public void setPurchased(boolean b) {
+		this.purchased = b;
+		
+	}
+
+}
